eedb65b34f59570887c1a2e8a1ec38951c7df95a
Andrzej Kopeć <kapkowski@gmail.com>
2015-02-24 20:08:13 +0100

    initial commit - probably .idea dir needs to be removed

---
+++ reference-calc/src/com/reference/calc/Main.java
@@ -0,0 +1,10 @@
+package com.reference.calc;
+
+/**
+ * Created by kapke on 24.02.15.
+ */
+public class Main {
+    public static void main (String[] args) {
+        System.out.println("Ala ma kota");
+    }
+}
